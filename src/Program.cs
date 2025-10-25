b69c36aa3bc1d0ba047f6a7ceb0363432ac9642a
DIOGO DA S RIBEIRO <diogos_ribeiro@outlook.com>
2025-10-25 20:16:37 -0300

    Finalizado - Caça aos Bugs 2025 - Desafio 01 - Spore

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services
+        .AddDbContext<AppDbContext>(x => x.UseSqlite(connString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
